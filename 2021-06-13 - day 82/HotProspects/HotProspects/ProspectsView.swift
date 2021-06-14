3785626aff80078e223eacaeef0399acb97de80b
Brandon Stephens <brandonstephens@me.com>
2021-06-14 08:25:50 -0400

    day 82

---
+++ 2021-06-13 - day 82/HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,74 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Brandon Stephens on 6/14/21.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    @EnvironmentObject var prospects: Prospects
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var filteredProspects: [Prospect] {
+        switch filter {
+        case .none:
+            return prospects.people
+        case .contacted:
+            return prospects.people.filter { $0.isContacted }
+        case .uncontacted:
+            return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(filteredProspects) { prospect in
+                    VStack(alignment: .leading) {
+                        Text(prospect.name)
+                            .font(.headline)
+                        
+                        Text(prospect.emailAddress)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationBarTitle(title)
+            .navigationBarItems(trailing: Button(action: {
+                let prospect = Prospect()
+                prospect.name = "Brandon"
+                prospect.emailAddress = "brandon@example.com"
+                prospect.isContacted = true
+                
+                self.prospects.people.append(prospect)
+            }, label: {
+                Image(systemName: "qrcode.viewfinder")
+                Text("Scan")
+            }))
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
